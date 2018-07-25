697ccaf74f49c7465c23b693d9cc3cd1ebd8a289
pierre.plagnes@gmail.com <pierre.plagnes@gmail.com>
2018-07-25 10:16:13 +0200

    initial version

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Information
+Just a simple .net library that allows to write formated text in the console
---
+++ src/ColorConsole/ColorConsole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ColorConsole
+{
+    /// <summary>
+    /// The main class of the library
+    /// Allows to interact with the console
+    /// </summary>
+    public static class CConsole
+    {
+        /// <summary>
+        /// Used to write a line in the console
+        /// </summary>
+        /// <param name="value">The text to write</param>
+        /// <param name="color">The color that must be used</param>
+        public static void WriteLine(string value, ConsoleColor color=ConsoleColor.White)
+        {
+            var previousColor = Console.ForegroundColor;
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(value);
+            }
+            finally
+            {
+                Console.ForegroundColor = previousColor;
+            }
+        }
+        /// <summary>
+        /// Used to write text in the console
+        /// </summary>
+        /// <param name="value">The text to write</param>
+        /// <param name="color">The color that must be used</param>
+        public static void Write(string value, ConsoleColor color = ConsoleColor.White)
+        {
+            var previousColor = Console.ForegroundColor;
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.Write(value);
+            }
+            finally
+            {
+                Console.ForegroundColor = previousColor;
+            }
+        }
+    }
+}
